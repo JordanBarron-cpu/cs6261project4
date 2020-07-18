b0ca1c79c04a10b696d47b9ae51e0181fa726bdf
jbarron8 <jbarron8@cs6261-vm07.uwg.westga.edu>
2020-07-18 15:55:06 -0400

    Able to iterate through colors array successfully. Need to create model then set up sigFig, multiplier, and tolerance branches

--- src/app/resistance/resistance.component.ts
+++ src/app/resistance/resistance.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resistance.component.css']
 })
 export class ResistanceComponent implements OnInit {
+  public colors: string[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue',
+                'violet', 'grey', 'white', 'gold', 'silver', 'none'];
+  }
+
+  trackColorBySlot(index, color) {
+    return color.index;
   }
 
 }
