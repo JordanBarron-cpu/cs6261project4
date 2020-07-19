7228ce8ea570cb023438cce8403d17f229596827
jbarron8 <jbarron8@cs6261-vm01.uwg.westga.edu>
2020-07-19 00:19:50 -0400

    Resistance component layout set. Beginning creation of service

--- src/app/resistance/resistance.component.ts
+++ src/app/resistance/resistance.component.ts
@@ -7,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResistanceComponent implements OnInit {
   public colors: string[];
+
   public sigFiguresList: string[];
   public multiplierList: string[];
-  public tolerance: number[];
+  public toleranceList: string[];
+
   public sigFiguresDict: any;
+  public multiplerDict: any;
+  public toleranceDict: any;
+
 
   constructor() { }
 
@@ -27,7 +32,12 @@ export class ResistanceComponent implements OnInit {
                           this.colors[6], this.colors[7], this.colors[8],
                           this.colors[9], this.colors[10], this.colors[11]
                           ];
-    this.tolerance = [];
+    this.toleranceList = [this.colors[1], this.colors[2],
+                          this.colors[5], this.colors[6],
+                          this.colors[7], this.colors[8],
+                          this.colors[10], this.colors[11],
+                          this.colors[12]
+                          ];
 
     this.sigFiguresDict = {
                     'black': 0, 'brown': 1,
@@ -37,13 +47,22 @@ export class ResistanceComponent implements OnInit {
                     'grey': 8, 'white': 9
                   };
 
-    this.sigFiguresDict = {
-                    'black': 0, 'brown': 1,
-                    'red': 2, 'orange': 3,
-                    'yellow': 4, 'green': 5,
-                    'blue': 6, 'violet': 7,
-                    'grey': 8, 'white': 9
-                    };
+    this.multiplerDict = {
+                    'black': 'x1', 'brown': 'x10',
+                    'red': 'x100', 'orange': 'x1K',
+                    'yellow': 'x10K', 'green': 'x100K',
+                    'blue': 'x1M', 'violet': 'x10M',
+                    'grey': 'x100M', 'white': 'x1G',
+                    'gold': 'x0.1', 'silver': 'x0.01'
+                  };
+
+    this.toleranceDict = {
+                    'brown': 1, 'red': 2,
+                    'green': .5, 'blue': .25,
+                    'violet': .1, 'grey': 0.05,
+                    'gold': 5, 'silver': 10,
+                    'none': 20
+                  };
   }
 
   getColors(): string[] {
@@ -58,24 +77,23 @@ export class ResistanceComponent implements OnInit {
     return this.multiplierList;
   }
 
-  getTolerance(): number[] {
-    return this.tolerance;
+  getToleranceList(): string[] {
+    return this.toleranceList;
   }
 
   getSigFiguresDict(color: string) : any {
     return this.sigFiguresDict[color];
   }
-  //
-  // getSigFiguresKeys(): string[] {
-  //   return this.sigFiguresDict.keys();
-  // }
-  //
-  // getSigFiguresValues(): number[] {
-  //   return this.sigFiguresDict.values();
-  // }
 
   trackSigFigBySlot(index, sigFiguresList) {
     return sigFiguresList.index;
   }
 
+  trackMultiplierBySlot(index, multiplierList) {
+    return multiplierList.index;
+  }
+
+  trackToleranceBySlot(index, toleranceList) {
+    return toleranceList.index;
+  }
 }
