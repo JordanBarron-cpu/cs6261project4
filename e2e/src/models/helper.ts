d6df56ce90a6b8b323a7393dfe1dd8818c9f94a6
jbarron8 <jbarron8@cs6261-vm02.uwg.westga.edu>
2020-07-20 18:15:57 -0400

    Working on correctIdFor sig figures

--- e2e/src/models/helper.ts
+++ e2e/src/models/helper.ts
@@ -4,12 +4,26 @@ export class Helper {
 
   constructor() {}
 
+/**
+loads the home page
+*/
   loadHome() {
-    browser.get('/');
+    browser.get('/index.html');
   }
 
-  idTextExpectation(idName: string, textExpectation: string) {
+  /**
+  returns the text of an id
+  */
+  getIdTextExpectation(idName: string, textExpectation: string) {
     let actualIdText = element(by.id(idName)).getText();
     expect(actualIdText).toEqual(textExpectation);
+  }
+
+  /**
+  returns the value of an id
+  */
+  getIdValueExpectation(idName: string, valueExpectation: string) {
+    let actualIdValue = element(by.id(idName)).getAttribute('value');
+    expect(actualIdValue).toEqual(valueExpectation);
   }
 }
--- e2e/src/success.e2e-spec.ts
+++ e2e/src/success.e2e-spec.ts
@@ -6,6 +6,22 @@ describe('successful scenario tests', () => {
 
   it('load Home', () => {
     helper.loadHome();
-    helper.idTextExpectation('resistanceMessage', 'Resistance:');
+    helper.getIdTextExpectation('resistanceMessage', 'Resistance:');
+  });
+
+  it('initial value and text of selectBoxes', () => {
+    helper.loadHome();
+
+    // helper.getIdValueExpectation('sigFig1', '0');
+    // helper.getIdValueExpectation('sigFig2', '0');
+    // helper.getIdValueExpectation('sigFig3', '0');
+    helper.getIdValueExpectation('multiplier0', '1');
+    helper.getIdValueExpectation('tolerance0', '1');
+
+    helper.getIdTextExpectation('sigFig0', 'black');
+    // helper.getIdTextExpectation('sigFig2', 'black');
+    // helper.getIdTextExpectation('sigFig3', 'black');
+    helper.getIdTextExpectation('multiplier0', 'x1');
+    helper.getIdTextExpectation('tolerance0', '1%');
   });
 });
