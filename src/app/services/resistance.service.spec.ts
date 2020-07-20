679470b758b69f88ae8708aa2ef4db499d83e1b2
jbarron8 <jbarron8@cs6261-vm17.uwg.westga.edu>
2020-07-20 03:34:59 -0400

    All resistance service tests created, tested, and passed. Need to complete component tests, e2e tests, and deploy

--- src/app/services/resistance.service.spec.ts
+++ src/app/services/resistance.service.spec.ts
@@ -23,4 +23,61 @@ describe('ResistanceService', () => {
     expect(service.multiplierResistanceBuild).toEqual(1);
     expect(service.multiplierUnitResistanceBuild).toEqual('');
   });
+
+  it('should set and get values correctly', () => {
+    expect(service.getSigFigure1ResistanceBuild()).toEqual(0);
+    service.addSigFigure1ResistanceBuild(9);
+    expect(service.getSigFigure1ResistanceBuild()).toEqual(9);
+
+    expect(service.getSigFigure2ResistanceBuild()).toEqual(0);
+    service.addSigFigure2ResistanceBuild(5);
+    expect(service.getSigFigure2ResistanceBuild()).toEqual(5);
+
+    expect(service.getSigFigure3ResistanceBuild()).toEqual(0);
+    service.addSigFigure3ResistanceBuild(2);
+    expect(service.getSigFigure3ResistanceBuild()).toEqual(2);
+
+    expect(service.getToleranceResistanceBuild()).toEqual(0);
+    service.addToleranceResistanceBuild(0.5);
+    expect(service.getToleranceResistanceBuild()).toEqual(0.5);
+
+    expect(service.getMultiplierResistanceBuild()).toEqual(1);
+    service.addMultiplierResistanceBuild(100);
+    expect(service.getMultiplierResistanceBuild()).toEqual(100);
+
+    expect(service.getMultiplierUnitResistanceBuild()).toEqual('');
+    service.addMultiplierUnitResistanceBuild('G');
+    expect(service.getMultiplierUnitResistanceBuild()).toEqual('G');
+  });
+
+  it('should calculate resistance using significant figures and multiplier', () => {
+    service.addSigFigure1ResistanceBuild(4);
+    service.addSigFigure2ResistanceBuild(5);
+    service.addSigFigure3ResistanceBuild(6);
+    service.addMultiplierResistanceBuild(1);
+    expect(service.getCalculation()).toEqual(456);
+
+    service.addSigFigure1ResistanceBuild(1);
+    service.addSigFigure2ResistanceBuild(2);
+    service.addSigFigure3ResistanceBuild(3);
+    service.addMultiplierResistanceBuild(10);
+    expect(service.getCalculation()).toEqual(1230);
+
+    service.addSigFigure1ResistanceBuild(7);
+    service.addSigFigure2ResistanceBuild(8);
+    service.addSigFigure3ResistanceBuild(9);
+    service.addMultiplierResistanceBuild(100);
+    expect(service.getCalculation()).toEqual(78900);
+  });
+
+  it('should display full string of the Resistance build', () => {
+    service.addSigFigure1ResistanceBuild(2);
+    service.addSigFigure2ResistanceBuild(4);
+    service.addSigFigure3ResistanceBuild(5);
+    service.addMultiplierResistanceBuild(10);
+    service.addMultiplierUnitResistanceBuild('M');
+    service.addToleranceResistanceBuild(0.25);
+
+    expect(service.calculationToString()).toEqual("2450M +/- 0.25%");
+  });
 });
