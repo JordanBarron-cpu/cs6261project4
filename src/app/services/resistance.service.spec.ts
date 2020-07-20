cbf71d5fed8b68d1f7b250509dd7bb898e353719
jbarron8 <jbarron8@cs6261-vm17.uwg.westga.edu>
2020-07-20 02:33:05 -0400

    initialization tests set for service

--- src/app/services/resistance.service.spec.ts
+++ src/app/services/resistance.service.spec.ts
@@ -13,4 +13,14 @@ describe('ResistanceService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should initialize all values correctly', () => {
+    expect(service.theCalculation).toEqual(0);
+    expect(service.sigFigure1ResistanceBuild).toEqual(0);
+    expect(service.sigFigure2ResistanceBuild).toEqual(0);
+    expect(service.sigFigure3ResistanceBuild).toEqual(0);
+    expect(service.toleranceResistanceBuild).toEqual(0.0);
+    expect(service.multiplierResistanceBuild).toEqual(1);
+    expect(service.multiplierUnitResistanceBuild).toEqual('');
+  });
 });
