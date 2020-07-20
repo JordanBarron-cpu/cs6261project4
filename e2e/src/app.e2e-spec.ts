3edc859dba45f9f6eff23bc830d4674f6de16534
jbarron8 <jbarron8@cs6261-vm02.uwg.westga.edu>
2020-07-20 15:53:51 -0400

    fixed app.po.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,4 +7,9 @@ describe('workspace-project App', () => {
   beforeEach(() => {
     page = new AppPage();
   });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getTitleText()).toEqual('Welcome to Resistance Calculator!');
+  });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -2,10 +2,10 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+    return browser.get(browser.baseUrl) as Promise<any>;
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('app-root h1')).getText() as Promise<string>;
   }
 }
